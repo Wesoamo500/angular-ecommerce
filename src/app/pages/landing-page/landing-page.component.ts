7c9b11a528f736c815c60bfe8047d934179e0b5d
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-24 17:13:03 +0000

    feat: add landing page

---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+}
