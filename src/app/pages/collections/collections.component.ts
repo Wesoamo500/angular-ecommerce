4128613fe2db6feda7c2498b5566bb000397b3a2
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-24 18:39:23 +0000

    ref: modularize collection component

--- src/app/pages/collections/collections.component.ts
+++ src/app/pages/collections/collections.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { CurrencyPipe, PercentPipe, UpperCasePipe } from '@angular/common';
+import { ImageBtnComponent } from '../../components/image-btn/image-btn.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { Products } from '../../../data/products';
+import { CartService } from '../../services/cart.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-collections',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, NavbarComponent, ImageBtnComponent, UpperCasePipe, CurrencyPipe, PercentPipe],
   templateUrl: './collections.component.html',
   styleUrl: './collections.component.css'
 })
 export class CollectionsComponent {
-
+  router = inject(Router)
+  cartService = inject(CartService)
+  authService = inject(AuthService)
+  
+  products = Products
+  active = 0;
+  isActive = true;
+  cartProducts:number[] = []
+  handleIsActive(index: number){
+    return this.active === index
+  }
+  hangeProductChange(index: number) {
+    this.active = index
+  }
+  decrease(index:number) {
+    this.products.map((product, i)=>{
+      if(index === i) product.quantity--
+    })
+  }
+  increase(index:number) {
+    this.products.map((product, i)=>{
+      if(index === i) product.quantity++
+    })
+  }
+  handleAddToCart(productId: number) {
+    if(this.authService.checkAuth()){
+      this.router.navigateByUrl('auth/login')
+    }
+    if(this.cartProducts.includes(productId)) return
+    this.cartProducts = [...this.cartProducts, productId]
+    this.cartService.updateNumberOfProducts(this.cartProducts.length)
+  }
 }
