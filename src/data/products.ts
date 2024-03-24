4b55b47758fb7fca16e0b3654346d97fd2716a2c
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-24 18:40:33 +0000

    feat: add product services

---
+++ src/data/products.ts
@@ -0,0 +1,33 @@
+export const Products = [
+    {
+      productId: 1,
+      company:'Sneaker Company',
+      title:'Fall Limited Edition Sneakers',
+      description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+      price: 250,
+      discountPercent: 0.5,
+      quantity: 0,
+      images:[
+        {
+          id: 1,
+          image: "../assets/images/image-product-1.jpg",
+          thumbnail: "../assets/images/image-product-1-thumbnail.jpg"
+        },
+        {
+          id: 2,
+          image: "../assets/images/image-product-2.jpg",
+          thumbnail: "../assets/images/image-product-2-thumbnail.jpg"
+        },
+        {
+          id: 3,
+          image: "../assets/images/image-product-3.jpg",
+          thumbnail: "../assets/images/image-product-3-thumbnail.jpg"
+        },
+        {
+          id: 4,
+          image: "../assets/images/image-product-4.jpg",
+          thumbnail: "../assets/images/image-product-4-thumbnail.jpg"
+        }
+      ]
+    }
+  ]
