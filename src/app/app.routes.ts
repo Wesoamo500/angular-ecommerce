388a69b549eac899be2b60df1ed5ea691237f2cc
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-24 18:38:33 +0000

    ref: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { MenSectionsComponent } from './pages/men-sections/men-sections.component';
+import { WomenSectionsComponent } from './pages/women-sections/women-sections.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
-    {path:'collections', component: AppComponent},
-    {path: 'men-section', component: AppComponent},
-    {path: 'women-section', component: AppComponent},
-    {path: 'about', component: AppComponent},
-    {path: 'contact', component: AppComponent}
+    { path: '', component: LandingPageComponent },
+    { path: 'collections', component: CollectionsComponent },
+    { path: 'men-section', component: MenSectionsComponent },
+    { path: 'women-section', component: WomenSectionsComponent },
+    { path: 'auth/login', component: AuthComponent },
+    { path: 'auth/register', component: AuthComponent },
+    { path: '**', component: NotFoundComponent },
 ];
