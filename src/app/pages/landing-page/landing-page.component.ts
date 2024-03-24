865852dc6226fb650ea3ffd833f4773c1224050a
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-24 18:39:46 +0000

    ref: update the landing page design

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { CollectionsComponent } from "../collections/collections.component";
 
 @Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+    selector: 'app-landing-page',
+    standalone: true,
+    templateUrl: './landing-page.component.html',
+    styleUrl: './landing-page.component.css',
+    imports: [CollectionsComponent]
 })
 export class LandingPageComponent {
 }
