2a020f1a003c53c0d2ebcceb07f12b8e50ae1835
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-26 09:18:42 +0000

    ref: add other attributes to the product schema

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,6 +4,23 @@ export const Products = [
       company:'Sneaker Company',
       title:'Fall Limited Edition Sneakers',
       description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+      reviews:[{
+        comment:"This Product was a good one 👍",
+        replies:{
+          Marvin: "It was very nice ❣️",
+          Kwaku: "I bought even two",
+          Sampson: "My girlfriend really loved it"
+        }
+      },
+      {
+        comment:"Bad product, the delivery system was bad, I wish I didn't order it.",
+        replies:{
+          Marvin: "Falsy 👎",
+          Ben: "Shut up troll",
+          Musky: "My girlfriend really loved it"
+        }
+      }],
+      tags:['cloth, sneakers, shirts'],
       price: 250,
       discountPercent: 0.5,
       quantity: 0,
