b107234f62a417ca4ac5169c5b402405bd0a99db
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-26 14:14:17 +0000

    ref: remove unused models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CartService } from './services/cart.service';
