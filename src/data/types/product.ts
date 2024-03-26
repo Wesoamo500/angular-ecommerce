aa29f1f6c84e93e75590c2b87ea61877fd0d529e
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-26 10:37:40 +0000

    feat: add types for product

---
+++ src/data/types/product.ts
@@ -0,0 +1,30 @@
+interface ReviewReplies {
+    [key: string]: string | undefined; // Key-value pairs where the key is a string (name) and the value is also a string (reply)
+  }
+  
+  // Interface for each review object
+  interface Review {
+    comment: string;
+    replies: ReviewReplies;
+  }
+  
+  // Interface for each image object
+  interface Image {
+    id: number;
+    image: string;
+    thumbnail: string;
+  }
+  
+  // Interface for the product object
+ export interface Product {
+    productId: number;
+    company: string;
+    title: string;
+    description: string;
+    reviews: Review[]; // Array of Review objects
+    tags: string[];
+    price: number;
+    discountPercent: number;
+    quantity: number;
+    images: Image[];
+  }
