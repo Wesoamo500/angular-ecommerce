6a6a4c8f6b5a2881d949c726edda6bff0cdfaeac
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-26 09:17:30 +0000

    ref: add some sections to the landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { CollectionsComponent } from "../collections/collections.component";
+import { ProductService } from '../../services/product.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.css',
-    imports: [CollectionsComponent]
+    imports: [CollectionsComponent, JsonPipe]
 })
 export class LandingPageComponent {
+    productService = inject(ProductService)
+
+    productDescription = this.productService.products[0].description;
+    reviews = this.productService.products[0].reviews
+
+    active = 'description';
+    onToggle(){
+        return `${this.active}Style`
+    }
+    handleToggle(type: string){
+        this.active = type;
+    }
+    reviewStyle(){
+        return 'text-[#3e3e3e]'
+    }
+    descriptionStyle(){
+        return 'text-[#ff7900]'
+    }
 }
