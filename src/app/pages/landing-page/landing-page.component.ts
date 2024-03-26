bee2abf25d2a2dea08537a210061e396352e7190
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-26 10:37:00 +0000

    ref: update the product card section

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -2,20 +2,21 @@ import { Component, inject } from '@angular/core';
 import { CollectionsComponent } from "../collections/collections.component";
 import { ProductService } from '../../services/product.service';
 import { JsonPipe } from '@angular/common';
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.css',
-    imports: [CollectionsComponent, JsonPipe]
+    imports: [CollectionsComponent, JsonPipe, ProductCardComponent]
 })
 export class LandingPageComponent {
     productService = inject(ProductService)
 
     productDescription = this.productService.products[0].description;
     reviews = this.productService.products[0].reviews
-
+    products = this.productService.products!
     active = 'description';
     onToggle(){
         return `${this.active}Style`
