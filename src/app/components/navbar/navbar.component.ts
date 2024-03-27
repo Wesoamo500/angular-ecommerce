5a6367d4c74b1f4d128268f84ee11e54ae5c5d03
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-03-27 11:40:24 +0000

    ref: add function definition

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,21 +2,27 @@ import { Component, inject, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { AuthService } from '../../services/auth.service';
+import { CartItemComponent } from "../cart/cart-item.component";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    standalone: true,
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css',
+    imports: [RouterLink, RouterLinkActive, CartItemComponent]
 })
 export class NavbarComponent {
   cartService = inject(CartService);
   authService = inject(AuthService);
+  open=false
   numberOf(){
     return this.cartService.numberOfProducts
   }
   checkAuth() {
     return this.authService.checkAuth()
+  }
+
+  hideCartedItemsCard() {
+    this.open = !this.open
   }
 }
